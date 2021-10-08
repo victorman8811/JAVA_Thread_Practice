8fba823fc9107f4998effc1e7c1491a88247aef8
Victor <victorman8811@gmail.com>
2021-10-08 18:47:35 +0800

    java_part3 quiz1-1

--- src/main/java/com/systex/quiz/ch01/Quiz1_1.java
+++ src/main/java/com/systex/quiz/ch01/Quiz1_1.java
@@ -1,58 +1,70 @@
 package com.systex.quiz.ch01;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Comparator;
+
 
 public class Quiz1_1 {
 
     public String getValueByIndex(List<String> list, int index) {
         // Todo Case
-        return "";
+        return list.get(index);
     }
 
     public List<String> insertValueToSpecificIndex(List<String> list, int index, String value) {
         // Todo Case
-        return Collections.emptyList();
+        list.add(index, value);
+        return list;
     }
 
     public List<String> updateValueByIndex(List<String> list, int index, String value) {
         // Todo Case
-        return Collections.emptyList();
+        list.set(index, value);
+        return list;
     }
 
     public List<String> removeValueByIndex(List<String> list, int index) {
         // Todo Case
-        return Collections.emptyList();
+        list.remove(index);
+        return list;
     }
 
     public boolean checkValueExists(List<String> list, String value) {
         // Todo Case
-        return false;
+        return list.contains(value);
     }
 
     public boolean checkListEmpty(List<String> list) {
         // Todo Case
-        return false;
+        return list.isEmpty();
     }
 
     public List<String> sortList(List<String> list) {
         // Todo Case
         return Collections.emptyList();
+
     }
 
     public List<String> copyList(List<String> list) {
         // Todo Case
-        return Collections.emptyList();
+        ArrayList<String> ans = new ArrayList<String>();
+        ans.addAll(list);
+        return ans;
     }
 
     public List<String> extractList(List<String> list, int fromIndex, int toIndex) {
         // Todo Case
-        return Collections.emptyList();
+        return list.subList(fromIndex,toIndex);
     }
 
     public List<String> concatList(List<String> list1, List<String> list2) {
         // Todo Case
-        return Collections.emptyList();
+        ArrayList<String> ans = new ArrayList<String>();
+        ans.addAll(list1);
+        ans.addAll(list2);
+        return ans;
     }
 
     public int maxNumber(List<Integer> list) {
