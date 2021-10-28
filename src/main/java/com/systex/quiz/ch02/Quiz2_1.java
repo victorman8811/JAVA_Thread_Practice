456d731befd7d6f9ed1f7236ea24d3ec52b7b2af
Victor <victorman8811@gmail.com>
2021-10-28 16:02:02 +0800

    java part3 quiz2-1

--- src/main/java/com/systex/quiz/ch02/Quiz2_1.java
+++ src/main/java/com/systex/quiz/ch02/Quiz2_1.java
@@ -1,32 +1,52 @@
 package com.systex.quiz.ch02;
 
-import java.util.Collection;
+import java.util.*;
+import java.util.stream.*;
 
 public class Quiz2_1 {
 
     public int maxNumber(Collection<Integer> c) {
         // Todo Case
-        return 0;
+        Integer ans = c.stream().max(Integer::compare).get();
+        return ans;
     }
 
     public int sumAllNumbers(Collection<Integer> c) {
         // Todo Case
-        return 0;
+        Integer sum = c.stream().mapToInt(Integer::intValue).sum();
+        return sum;
     }
 
     public int itemCounts(Collection<Integer> c) {
         // Todo Case
-        return 0;
+        long temp = c.stream().count();
+        int ans = (int) temp;
+        return ans;
     }
 
     public boolean allNumbersBiggerThan10(Collection<Integer> c) {
         // Todo Case
-        return true;
+        List<Integer> ans = new ArrayList<Integer>();
+        ans = c.stream().filter(s -> s.intValue() < 10).collect(Collectors.toList());
+        if (ans.isEmpty() == false) {
+
+            return false;
+
+        } else
+            return true;
     }
 
     public boolean anyNumberBiggerThan10(Collection<Integer> c) {
         // Todo Case
-        return true;
+        List<Integer> ans = new ArrayList<Integer>();
+        ans = c.stream().filter(s -> s.intValue() > 10).collect(Collectors.toList());
+        if (ans.isEmpty() == true) {
+
+            return false;
+
+        } else
+            return true;
+
     }
 
 }
