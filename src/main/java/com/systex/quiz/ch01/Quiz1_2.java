974c3c0e581c26e5b43c47dd47784387418f40a0
Victor <victorman8811@gmail.com>
2021-10-28 16:01:20 +0800

    java part3 quiz1-2

--- src/main/java/com/systex/quiz/ch01/Quiz1_2.java
+++ src/main/java/com/systex/quiz/ch01/Quiz1_2.java
@@ -1,53 +1,56 @@
 package com.systex.quiz.ch01;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Quiz1_2 {
 
     public Set<String> insertValue(Set<String> set, String value) {
         // Todo Case
-        return Collections.emptySet();
+        set.add(value);
+        return set;
     }
 
     public Set<String> removeValue(Set<String> set, String value) {
         // Todo Case
-        return Collections.emptySet();
+        set.remove(value);
+        return set;
     }
 
     public boolean checkValueExists(Set<String> set, String value) {
         // Todo Case
-        return false;
+        return set.contains(value);
     }
 
     public boolean checkSetEmpty(Set<String> set) {
         // Todo Case
-        return false;
+        return set.isEmpty();
     }
 
     public Set<String> copySet(Set<String> set) {
         // Todo Case
-        return Collections.emptySet();
+        Set<String> ans = new HashSet<String>();
+        ans.addAll(set);
+        return ans;
     }
 
     public List<String> covertToList(Set<String> set) {
         // Todo Case
-        return Collections.emptyList();
+        List<String> ans=new ArrayList<>(set);
+        return ans;
     }
 
     public int maxNumber(Set<Integer> list) {
         // Todo Case
-        return 0;
+        return Collections.max(list);
     }
 
     public int sumAllNumbers(Set<Integer> list) {
         // Todo Case
-        return 0;
+        return list.stream().mapToInt(Integer::intValue).sum();
     }
 
     public int itemCounts(Set<Integer> set) {
         // Todo Case
-        return 0;
+        return set.size();
     }
 }
