ffad7437d458dace62850555bd30b3e4f6c67594
Victor <victorman8811@gmail.com>
2021-11-01 18:08:03 +0800

    java part3 quiz2-5 final

--- src/main/java/com/systex/quiz/ch02/Quiz2_5.java
+++ src/main/java/com/systex/quiz/ch02/Quiz2_5.java
@@ -1,10 +1,21 @@
 package com.systex.quiz.ch02;
 
+import java.util.*;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
 
 public class Quiz2_5 {
-    public Map<String, Long> mapReduceWordCount(String input){
+    public Map<String, Long> mapReduceWordCount(String input) {
         // TODO
-        return null;
+        input = input.replace("!", "");
+        input = input.replace(",", "");
+        List<String> myList = new ArrayList<String>(Arrays.asList(input.split(" ")));
+        Map<String, Long> ans = myList.stream().collect(groupingBy(Function.identity(), Collectors.counting()));
+        return ans;
     }
+
 }
