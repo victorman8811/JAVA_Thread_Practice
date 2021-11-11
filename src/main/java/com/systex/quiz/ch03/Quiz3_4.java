6367034ec2f56ef4a8fe6a7505b855a891b234a5
Victor <victorman8811@gmail.com>
2021-11-11 10:51:50 +0800

    java part3 quiz3-4 final

--- src/main/java/com/systex/quiz/ch03/Quiz3_4.java
+++ src/main/java/com/systex/quiz/ch03/Quiz3_4.java
@@ -2,41 +2,53 @@ package com.systex.quiz.ch03;
 
 import com.systex.quiz.ch03.util.HorseRacing;
 
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static java.lang.Thread.sleep;
 
 public class Quiz3_4 {
 
     public static void horseRacing() {
-        int horseAmount = 2;
+
+        Scanner scanner = new Scanner(System.in);
+        int horseAmount = scanner.nextInt();
         HorseRacing horseRacingUI = new HorseRacing(horseAmount);
         horseRacingUI.setVisible(true);
+        Thread horse[] = new Thread[horseAmount];
+        AtomicInteger isWin = new AtomicInteger(0);/////////////////判斷是否已經有馬到終點，有的話=1
 
         // Todo Case
-        Thread horse1 = new Thread(() -> {
-            try {
-                horseRacingUI.setHorsePosition(1, 50);
-                sleep(2000);
-                horseRacingUI.setHorsePosition(1, 100);
-                horseRacingUI.setMessage("Horse 1 win");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+        for (int i = 0; i < horseAmount; i++) {
+            int horseNum = i;
+            horse[i] = new Thread(() -> {
+                try {
+                    int positon = 0;
+                    int nextStep = 0;
 
-        Thread horse2 = new Thread(() -> {
-            try {
-                horseRacingUI.setHorsePosition(2, 10);
-                sleep(1000);
-                horseRacingUI.setHorsePosition(2, 70);
-                sleep(2000);
-                horseRacingUI.setHorsePosition(2, 100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+                    while (positon < 100) {
+                        positon = positon + nextStep;
+                        if (positon >= 100) {
+                            positon = 100;
+                            if (isWin.get() == 0) {
+                                isWin.getAndIncrement();
+                                horseRacingUI.setMessage("Horse " + horseNum + " win");
+                            }
+                        }
+                        horseRacingUI.setHorsePosition(horseNum+1, positon);
+                        nextStep = (int) (Math.random() * 100) + 1;
+                        sleep((int) (Math.random() * (3000 - 1000 + 1)) + 1000);
+                    }
 
-        horse1.start();
-        horse2.start();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            horse[i].start();
+
+        }
+
     }
 
     public static void main(String[] args) {
