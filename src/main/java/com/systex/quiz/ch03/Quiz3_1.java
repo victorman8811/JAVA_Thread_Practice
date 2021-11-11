41a965355e1c55c21171ba688197057a0d627110
Victor <victorman8811@gmail.com>
2021-11-11 10:52:12 +0800

    java part3 quiz3-1 final

--- src/main/java/com/systex/quiz/ch03/Quiz3_1.java
+++ src/main/java/com/systex/quiz/ch03/Quiz3_1.java
@@ -1,15 +1,24 @@
 package com.systex.quiz.ch03;
 
+import com.systex.quiz.ch03.util.PrinterA;
+import com.systex.quiz.ch03.util.PrinterB;
+
 public class Quiz3_1 {
 
     public void printFromThreadByExtend(String msg) {
         // Todo Case
-        System.out.println(msg);
+        Thread threadA = new PrinterA(msg);
+
+        threadA.start();
+
+
     }
 
     public void printFromThreadByImplement(String msg) {
         // Todo Case
-        System.out.println(msg);
+        Thread threadB = new Thread(new PrinterB(msg));
+
+        threadB.start();
     }
 
 }
