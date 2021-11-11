804d5eed1618295a491647ad2f7681062a42c3c1
Victor <victorman8811@gmail.com>
2021-11-11 10:52:42 +0800

    java part3 quiz3-2 final

--- src/main/java/com/systex/quiz/ch03/Quiz3_2.java
+++ src/main/java/com/systex/quiz/ch03/Quiz3_2.java
@@ -1,16 +1,38 @@
 package com.systex.quiz.ch03;
 
+import static java.lang.Thread.sleep;
+
 public class Quiz3_2 {
 
     public Thread happyNewYear() {
         Thread countDown = new Thread(() -> {
             // Todo Case
+            System.out.print("3...");
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            System.out.print("2...");
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            System.out.print("1...");
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            System.out.println("Happy New Year!");
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+            }
         });
         countDown.start();
         return countDown;
     }
 
-    public void eatHamburger () {
+    public void eatHamburger() {
         Thread burgerQueen = new Thread(() -> {
             System.out.println("Tom's burger is done!");
         });
@@ -18,7 +40,14 @@ public class Quiz3_2 {
 
         // Todo Case
 
+        try {
+            burgerQueen.join();
+        } catch (InterruptedException e) {
+        }
+
+
         System.out.println("*Tom is eating burger, Yum-yum...");
+
     }
 
 }
