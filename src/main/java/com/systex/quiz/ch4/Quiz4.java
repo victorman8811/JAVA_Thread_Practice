9e7e0551dccaf810ea577f224fd053604052315b
Victor <victorman8811@gmail.com>
2021-11-11 10:54:11 +0800

    java part3 quiz4 final

--- src/main/java/com/systex/quiz/ch4/Quiz4.java
+++ src/main/java/com/systex/quiz/ch4/Quiz4.java
@@ -1,6 +1,33 @@
 package com.systex.quiz.ch4;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 
 public class Quiz4 {
+
+    public static void main(String args[]) {
+
+        insert(1,"www","hh");
+    }
+
+    public static void insert(int task_seq,String task_name,String description){
+
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/postgres",
+                            "postgres", "victor8811");
+            stmt = c.createStatement();
+
+            String sql = "INSERT INTO task_table " +
+                    "VALUES ("+task_seq+",'"+task_name+"','"+description+"')";
+            stmt.executeUpdate(sql);
+        } catch (Exception e) {
+        }
+    }
 
 
 
