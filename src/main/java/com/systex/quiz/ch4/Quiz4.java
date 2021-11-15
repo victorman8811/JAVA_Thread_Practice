8f5625f345fb00c804a92ed75aab5a3b125c53a5
Victor <victorman8811@gmail.com>
2021-11-15 17:24:43 +0800

    java part3 quiz4 final

--- src/main/java/com/systex/quiz/ch4/Quiz4.java
+++ src/main/java/com/systex/quiz/ch4/Quiz4.java
@@ -1,17 +1,51 @@
 package com.systex.quiz.ch4;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+
+import java.sql.*;
 
 
 public class Quiz4 {
 
     public static void main(String args[]) {
 
-        insert(1,"www","hh");
+        //insert("www", "hh");
+        //delete(100);
+        //read("www", "hh");
+        //update(1,"yyy","ddd");
     }
 
-    public static void insert(int task_seq,String task_name,String description){
+    public static int insert(String task_name, String description) {
+
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        int id = 0;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/postgres",
+                            "postgres", "victor8811");
+
+
+            String sql = "INSERT INTO task_table (task_name,description) VALUES (?,?)";
+            ps = c.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1, task_name);
+            ps.setString(2, description);
+
+            ps.executeUpdate();
+
+            rs = ps.getGeneratedKeys();
+            while (rs.next()) {
+                id = rs.getInt(1);
+            }
+
+        } catch (Exception e) {
+        }
+        return id;
+    }
+
+    public static void delete(int task_seq) {
 
         Connection c = null;
         Statement stmt = null;
@@ -22,19 +56,70 @@ public class Quiz4 {
                             "postgres", "victor8811");
             stmt = c.createStatement();
 
-            String sql = "INSERT INTO task_table " +
-                    "VALUES ("+task_seq+",'"+task_name+"','"+description+"')";
+            String sql = "DELETE FROM task_table " +
+                    "WHERE task_seq = " + task_seq;
             stmt.executeUpdate(sql);
         } catch (Exception e) {
         }
     }
 
+    public static boolean read(int seq) {
 
+        Connection c = null;
+        Statement stmt = null;
+        boolean dataExist = false;
 
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/postgres",
+                            "postgres", "victor8811");
+            stmt = c.createStatement();
 
+            String sql = "SELECT task_seq, task_name, description FROM task_table WHERE task_seq = '" + seq + "'";
+            ResultSet rs = stmt.executeQuery(sql);
 
+            while (rs.next()) {
+                //Retrieve by column name
+                dataExist = true;
+                int task_seq = rs.getInt("task_seq");
+                String task_name = rs.getString("task_name");
+                String description = rs.getString("description");
 
+                //Display values
+                System.out.println("task_seq: " + task_seq);
+                System.out.println("task_name: " + task_name);
+                System.out.println("description: " + description);
 
+            }
+            if (dataExist==false){
+                return false;
+            }
+            rs.close();
+
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void update(int seq, String name, String description) {
+
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/postgres",
+                            "postgres", "victor8811");
+            stmt = c.createStatement();
+
+            String sql = "UPDATE task_table " +
+                    "SET task_name = '" + name + "',description = '" + description + "' WHERE task_seq = " + seq;
+            stmt.executeUpdate(sql);
+        } catch (Exception e) {
+        }
+    }
 
 
 }
--- src/test/java/com/systex/quiz/ch04/Quiz4Test.java
+++ src/test/java/com/systex/quiz/ch04/Quiz4Test.java
@@ -1,15 +1,62 @@
 package com.systex.quiz.ch04;
 
+import com.systex.quiz.ch4.Quiz4;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Quiz4Test {
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    void setStreams() {
+        System.setOut(new PrintStream(out));
+    }
+
+    @AfterEach
+    void restoreInitialStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void dataInsertAndRead() {
+        Quiz4 quiz = new Quiz4();
+        String task_name = "xx7";
+        String description = "uuu";
+
+        int seq = quiz.insert(task_name, description);
+        quiz.read(seq);
+        assertEquals("task_seq: " + seq + "\r\n" + "task_name: " + task_name + "\r\n" + "description: " + description + "\r\n", out.toString());
+    }
+
+    @Test
+    void dataDelete() {
+
+        Quiz4 quiz = new Quiz4();
+        int seq = 3;
+        quiz.delete(seq);
+        assertEquals(false, quiz.read(seq));
 
 
+    }
 
+    @Test
+    void dataUpdate() {
+        Quiz4 quiz = new Quiz4();
+        int seq = 1;
+        String task_name = "5x7";
+        String description = "uuu";
 
-
-
-
-
-
+        quiz.update(seq,task_name, description);
+        quiz.read(seq);
+        assertEquals("task_seq: " + seq + "\r\n" + "task_name: " + task_name + "\r\n" + "description: " + description + "\r\n", out.toString());
+    }
 
 
 }
